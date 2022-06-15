24b3ac8e473f34de6aa0ceeada5eb0c036494f37
Tom Tarpey <tomtarpeydev@gmail.comn>
2022-06-15 18:58:02 +0100

    end of gp part 1 axios get

--- html-local/index.js
+++ html-local/index.js
@@ -1,1 +1,22 @@
-console.log("Hello inside the console!");
+const smurflist = document.querySelector("#smurfs");
+console.log(smurflist);
+
+
+axios.get("http://localhost:3333/smurfs")
+.then((res) => {
+    console.log(res.data);
+    populateSmurfs(res.data);
+})
+.catch((err) => {
+    console.log(err);
+});
+
+
+function populateSmurfs(smurfArray) {
+    for(let smurf of smurfArray) {
+        let li = document.createElement("li");
+        let text = document.createTextNode(smurf.name);
+        li.appendChild(text);
+        smurflist.appendChild(li);
+    }
+}
